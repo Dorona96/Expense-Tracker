cdf0e136bb449ed629f5724138fb93387057f313
DORONA <46745390+Dorona96@users.noreply.github.com>
2021-07-30 17:41:43 +0300

    Add files via upload

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Chart.css'
+
+import ChartBar from './ChartBar';
+
+const Chart = props => {
+    const dataPintsValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMax= Math.max(...dataPintsValues);
+
+    return (<div className="chart">
+        {props.dataPoints.map(dataPoint =>
+            <ChartBar
+                key={dataPoint.label}
+                value={dataPoint.value}
+                maxValue={totalMax}
+                label={dataPoint.label} />)}
+    </div>);
+};
+
+export default Chart;
